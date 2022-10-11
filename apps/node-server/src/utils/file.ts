ad488f53b8be717acfccfe0c96eb0815e32a8b9e
NAN <54296915+1066726487@users.noreply.github.com>
2022-10-11 16:30:31 +0800

    feat: 添加七牛云文件储存

--- apps/node-server/src/utils/file.ts
+++ apps/node-server/src/utils/file.ts
@@ -8,17 +8,12 @@ import * as crypto from 'crypto';
 
 // 获取可读文件流的Hash值
 export const getReadStreamHash =  (stream: NodeJS.ReadStream, hashOption: {algorithm: string, options?:crypto.HashOptions} ): Promise<string> => new Promise((resolve) => {
-
   const hash = crypto.createHash('sha256');
-
   stream.on('data', (data) => {
-    console.log('aaaa');
     hash.update(data);
   })
-
   stream.on('end', () => {
     resolve(hash.digest('hex'));
   })
-
   stream.resume()
 })
